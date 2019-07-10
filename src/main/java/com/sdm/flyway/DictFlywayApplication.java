55d0297a2dfbd33ea9d66e6ccde791f934255e78
moonscoding <supermoon.development@gmail.com>
2019-07-11 00:46:41 +0900

    tomcat packaging

--- src/main/java/com/sdm/flyway/DictFlywayApplication.java
+++ src/main/java/com/sdm/flyway/DictFlywayApplication.java
@@ -2,9 +2,10 @@ package com.sdm.flyway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DictFlywayApplication {
+public class DictFlywayApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(DictFlywayApplication.class, args);
