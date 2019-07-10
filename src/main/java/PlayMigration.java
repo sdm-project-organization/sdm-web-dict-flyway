ecaa3c1de6dd15e6177de7a8df5298a48661ab9f
supermoon <supermoon@nexon.co.kr>
2019-07-11 00:11:26 +0900

    springboot update

--- src/main/java/PlayMigration.java
+++
@@ -1,9 +0,0 @@
-import config.FlywayConfig;
-import org.flywaydb.core.Flyway;
-
-public class PlayMigration {
-
-    public static void main(String[] args) {
-        Flyway flyway = FlywayConfig.getFlyway();
-    }
-}
--- src/main/java/config/FlywayConfig.java
+++ src/main/java/config/FlywayConfig.java
@@ -1,18 +1,44 @@
 package config;
 
 import org.flywaydb.core.Flyway;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+import javax.annotation.PostConstruct;
+
+@Configuration
 public class FlywayConfig {
 
-    public static final String HOST = "210.90.135.104:10002";
-    public static final String SCHEMA = "DICT";
-    public static final String ID = "root";
-    public static final String PW = "root";
-    public static final String LOCATION = "classpath:migration";
+    @Value("${flyway.url}")
+    public String HOST;
 
-    public static Flyway getFlyway() {
+    @Value("${flyway.port}")
+    public String PORT;
+
+    @Value("${flyway.schema}")
+    public String SCHEMA;
+
+    @Value("${flyway.username}")
+    public String ID;
+
+    @Value("${flyway.password}")
+    public String PW;
+
+    @Value("${flyway.location}")
+    public String LOCATION;
+
+    @PostConstruct
+    public void init() {
+        System.out.println("flywayConfig init");
+        getFlyway();
+    }
+
+    @Bean
+    public Flyway getFlyway() {
+        System.out.println(HOST);
         Flyway flyway = new Flyway();
-        flyway.setDataSource("jdbc:mysql://" + HOST + "/" + SCHEMA, ID, PW);
+        flyway.setDataSource("jdbc:mysql://" + HOST + ":" + PORT + "/" + SCHEMA, ID, PW);
         flyway.setLocations(LOCATION);
         flyway.clean();
         flyway.migrate();
